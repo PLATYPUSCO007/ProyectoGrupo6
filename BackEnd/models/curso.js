14480e8723a97ab9da144acda4a97ae0ee67ea88
Esteban Camacho Salgado <6esteban.dev@gmail.com>
2024-07-10 11:41:36 -0500

    feat: add models

---
+++ BackEnd/models/curso.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const CursoSchema = new mongoose.Schema({
+  id_usuario: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  titulo: {
+    type: String,
+    required: true,
+  },
+  categoria: {
+    type: String,
+  },
+  descripcion: {
+    type: String,
+    required: true,
+  },
+  duracion: {
+    type: Number,
+  },
+  imagen: {
+    type: String,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default model("Curso", CursoSchema, "curso");
