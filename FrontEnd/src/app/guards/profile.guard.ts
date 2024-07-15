f5f2b054f03512e3e2da1b1c37a6c25a83060b7c
PLATYPUSCO007 <william.enciso@cun.edu.co>
2024-07-15 14:27:29 -0500

    Hide Menu By Roles AND Protect Routes By Roles

---
+++ FrontEnd/src/app/guards/profile.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, GuardResult, MaybeAsync, Route, RouterStateSnapshot, UrlSegment } from '@angular/router';
+import { Observable } from 'rxjs';
+import { LoginService } from '../login/services/loginService/login.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProfileGuard implements CanActivate {
+
+    private loginService = inject(LoginService);
+
+    canActivate( route: ActivatedRouteSnapshot): Observable<boolean> | Promise<boolean> | boolean{
+        const profile = route.data['profile'];
+        const profileUser = this.loginService.userSession.name;
+        return (profile === profileUser);
+    }
+}
