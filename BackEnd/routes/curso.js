c7d6ab793defce9c4f23502f1e995baa405e1a58
Camilo <jcrp.002@gmail.com>
2024-07-25 13:48:31 -0500

    Update Curso and token in Curso.

--- BackEnd/routes/curso.js
+++ BackEnd/routes/curso.js
@@ -2,12 +2,13 @@
 import { Router } from "express";
 const router = Router();
 import { addUserToCurso, saveCurso, updateCurso } from "../controllers/curso.js";
+import { ensureAuth } from "../middleware/auth.js";
 
 
 //Rutas
-router.post('/register', saveCurso);
-router.post('/addcurso/:id', addUserToCurso);
-router.post('/updatecurso/:id', updateCurso);
+router.post('/register',ensureAuth, saveCurso);
+router.post('/addcurso/:id',ensureAuth, addUserToCurso);
+router.post('/updatecurso/:id',ensureAuth, updateCurso);
 
 // Exportar el Router
 export default router;
